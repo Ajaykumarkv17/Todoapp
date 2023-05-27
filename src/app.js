33f3823ca42ad186a85e57f64b9f9cba0d08f831
Ajay Kumar <ajaykumarkv0317@gmail.com>
2023-05-27 19:57:44 +0530

    oopp

--- src/app.js
+++ src/app.js
@@ -1,26 +1,9 @@
 const express = require('express');
-const mysql = require('mysql2');
 const bodyParser = require('body-parser');
 const todoRoutes = require('./routes/todoRoutes');
+
 const app = express();
 const port = 3000;
-
-// Database connection
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'ajaykumar17',
-  password: 'AJay$2022',
-  database: 'todoapp',
-});
-
-// Connect to the database
-connection.connect((err) => {
-  if (err) {
-    console.error('Error connecting to the database: ', err);
-    return;
-  }
-  console.log('Connected to the database');
-});
 
 // Middleware
 app.use(bodyParser.json());
@@ -28,12 +11,10 @@ app.use(express.static('public'));
 
 // Routes
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '../public/index.html');
-  });
+  res.sendFile(__dirname + '/public/index.html');
+});
 
-  app.use('/todos', todoRoutes);
-
-
+app.use('/todos', todoRoutes);
 
 // Start the server
 app.listen(port, () => {
